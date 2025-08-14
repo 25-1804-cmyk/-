3ea921a2053b509fe5fbd4935dbd76fc95019280
25-1804-cmyk <25-1804@swjb.hs.kr>
2025-08-14 18:47:19 +0900

    Add files via upload

---
+++ wwww/test02.js
@@ -0,0 +1,9 @@
+
+        let reimu = 1; // 값을 숫자로 할당, 변수의 자료형 숫자형
+        console.log("reimu ========= ");
+        console.log(typeof reimu);
+
+        let hakurei = "1"; // 값을 문자로 할당, 변수의 자료형 문자
+        console.log("hakurei ========= ");
+        console.log(typeof hakurei);
+        
